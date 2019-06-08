f65a340db4aba28446e47c8dc9839d7004ee12b4
kutlay1653 <kutlaysarimehmet@gmail.com>
2019-06-08 22:39:17 +0000

    rework

--- etab5-vendor.mk
+++ etab5-vendor.mk
@@ -1,12 +1,12 @@
 
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lava/IrisX8/proprietary/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/generalmobile/etab5/proprietary/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lava/IrisX8/proprietary/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/generalmobile/etab5/proprietary/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lava/IrisX8/proprietary/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/generalmobile/etab5/proprietary/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lava/IrisX8/proprietary/vendor,system/vendor)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lava/generalmobile/etab5/vendor,system/vendor)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lava/IrisX8/proprietary/xbin,system/xbin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/generalmobile/etab5/proprietary/xbin,system/xbin)
 
